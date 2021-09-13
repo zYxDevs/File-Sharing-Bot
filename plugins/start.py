5f9d22819b77b359d4041c746a9f42c73ce41e24
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-13 20:53:42 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -91,8 +91,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("😊 Tentang", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Tutup", callback_data = "close")
                 ]
             ]
         )
