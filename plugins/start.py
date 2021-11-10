a703b409db43d4ba68f3d530d6d22220e12ceb91
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-11-10 14:46:09 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,6 +145,7 @@ async def not_joined(client: Client, message: Message):
             ),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
+        parse_mode = "markdown"
         disable_web_page_preview = True
     )
 
